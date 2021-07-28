0e28e07678e9c279692ddb0a2dd0dfe2cfb68e53
myolisi <myolisi@ngubentombi.co.za>
2021-07-28 18:57:26 +0200

    Clean up.

--- webApi/GUI/middleware/getPersons.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export default  function({store}){
-    return axios.get('http://localhost:4567/persons').then((response) => {
-      store.commit('add', response.data)
-    })
-}
--- webApi/GUI/plugins/mixins.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-Vue.mixin({
-  fetch() {
-    console.log('add person mixin')
-  }
-})
